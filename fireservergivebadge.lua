ac3769a6a5c0ea5c32a384d088667772e7435e58
rileybeerblx2 <162878813+rileybeerblx2@users.noreply.github.com>
2024-03-31 12:46:35 +0800

    Create fireservergivebadge.lua

---
+++ fireservergivebadge.lua
@@ -0,0 +1,6 @@
+game:GetService("ReplicatedStorage").GiveBadge:FireServer(2124520719)
+--[[
+game:GetService("ReplicatedStorage").GiveBadge:FireServer(2124521474)
+game:GetService("ReplicatedStorage").GiveBadge:FireServer(2124521475)
+game:GetService("ReplicatedStorage").GiveBadge:FireServer(2124521476)
+]]
